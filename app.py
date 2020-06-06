db93fbd8519968fab041ddca3a439a05d8b8be2f
Vedprakash <Vedprakashtawar2016@gmail.com>
2020-06-06 17:05:34 +0630

    graph

--- app.py
+++ app.py
@@ -1,42 +1,25 @@
-from flask import Flask , render_template , request
+from flask import Flask, Markup, render_template
+
 app = Flask(__name__)
+'''
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
 
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]'''
+
+d = ['labels':[967.67, 1190.89, 1079.75, 1349.19,2328.91, 2504.28, 2873.83, 4764.87,4349.29, 6458.30, 9907, 16297],'values':[967.67, 1190.89, 1079.75, 1349.19,2328.91, 2504.28, 2873.83, 4764.87,4349.29, 6458.30, 9907, 16297]]
 @app.route('/')
-def hello_world():
-     return render_template('index.html')
-
-@app.route('/my_form', methods=['POST','GET'])
-def my_form():
-    if request.method == 'POST':
-        c6 = int(request.form['c6'])
-        c3 = int(request.form['c3'])
-        cpl = int(request.form['cpl'])
-        x,y = 'stp','wrsmcduo'
-        d={}
-        for i in y:
-            sum=0
-            for j in x:
-                if len(request.form[j+i])>0:
-                    if j=='s':
-                        sum=sum+c6*(int(request.form[j+i]))
-                    if j=='t':
-                        sum=sum+c3*(int(request.form[j+i]))
-                    if j=='p':
-                        sum=sum+cpl*(int(request.form[j+i]))
-            d[i] = sum/1000
-        result = {}
-        for k,v in d.items():
-
-            if k == 'w':result['Wheat'] = v
-            if k == 'r':result['Rice'] = v
-            if k == 's':result['Soyabean'] = v
-            if k == 'm':result['Murmura'] = v
-            if k == 'c':result['Chna'] = v
-            if k == 'd':result['Dal'] = v
-            if k == 'u':result['Sugar'] = v
-            if k == 'o':result['Oil'] = v
-
-        return render_template('res.html',result=result)
+def line():
+    line_labels=d['labels']
+    line_values=d['values']
+    return render_template('line_chart.html', title='Covid19', max=17000, labels=line_labels, values=line_values)
 
 if __name__ == '__main__':
     app.run()
---
+++ app2.py
@@ -0,0 +1,42 @@
+from flask import Flask , render_template , request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+     return render_template('index.html')
+
+@app.route('/my_form', methods=['POST','GET'])
+def my_form():
+    if request.method == 'POST':
+        c6 = int(request.form['c6'])
+        c3 = int(request.form['c3'])
+        cpl = int(request.form['cpl'])
+        x,y = 'stp','wrsmcduo'
+        d={}
+        for i in y:
+            sum=0
+            for j in x:
+                if len(request.form[j+i])>0:
+                    if j=='s':
+                        sum=sum+c6*(int(request.form[j+i]))
+                    if j=='t':
+                        sum=sum+c3*(int(request.form[j+i]))
+                    if j=='p':
+                        sum=sum+cpl*(int(request.form[j+i]))
+            d[i] = sum/1000
+        result = {}
+        for k,v in d.items():
+
+            if k == 'w':result['Wheat'] = v
+            if k == 'r':result['Rice'] = v
+            if k == 's':result['Soyabean'] = v
+            if k == 'm':result['Murmura'] = v
+            if k == 'c':result['Chna'] = v
+            if k == 'd':result['Dal'] = v
+            if k == 'u':result['Sugar'] = v
+            if k == 'o':result['Oil'] = v
+
+        return render_template('res.html',result=result)
+
+if __name__ == '__main__':
+    app.run()
