28b21284fa9d74e26a78fd7f6869f79b49cc9fef
Vedprakash <Vedprakashtawar2016@gmail.com>
2020-06-23 02:30:38 +0630

    add forecasting data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, Markup, render_template
 import json
-
+import copy
 app = Flask(__name__)
 with open('tableData.json') as f:
   data = json.load(f)
@@ -10,6 +10,10 @@ with open('tableData.json') as f:
 
 with open('GraphData.json') as f:
   gdata = json.load(f)
+
+with open('ForecastData.json') as f:
+  fdata = json.load(f)
+
 t_c = 0
 t_a = 0
 t_r = 0
@@ -22,7 +26,22 @@ for j in st:
             l.append([i['date'],i['cum_confirmed'],i['cum_active'],i['cum_recovered']])
     g_data[j] = l
     l = []
+
+length = len(g_data['Bihar'])
+
+f_data = copy.deepcopy(g_data)
+
+for j in st:
+    for i in fdata:
+        if i['state'] == j:
+            f_data[j].append([i['date'],i['cum_confirmed'],i['cum_active'],i['cum_recovered']])
+
 g1_data   = g_data 
+
+for i in g_data.keys():
+    for j in range(11):
+        g_data[i].append(['null','null','null','null'])
+
 for i in data:
     t_c = t_c+int(i['cum_confirmed'])
     t_a = t_a+int(i['cum_active'])
@@ -30,7 +49,7 @@ for i in data:
 total = [t_c,t_a,t_r]
 @app.route('/')
 def line():
-    return render_template('graph.html', title='Covid19',table = data,total = total,gdata = g_data,st = st,g1data = g1_data)
+    return render_template('graph.html', title='Covid19',table = data,total = total,gdata = g_data,st = st,g1data = g1_data,fdata = f_data)
 
 if __name__ == '__main__':
     app.run()
