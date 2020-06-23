00c5b002a6e5bfcd2a64649820431d5668948781
Vedprakash <Vedprakashtawar2016@gmail.com>
2020-06-23 18:21:32 +0630

    .

--- app.py
+++ app.py
@@ -72,8 +72,6 @@ for i in g1_data.keys():
         for j in range(11):
             g1_data[i].append(['null','null','null'])
 
-
-print(f1_data['Bihar'])
 for i in data:
     t_c = t_c+int(i['cum_confirmed'])
     t_a = t_a+int(i['cum_active'])
@@ -81,11 +79,11 @@ for i in data:
 
 
 total = [t_c,t_a,t_r]
-tav = "<tr><td>{{ day }}</td><td class='freq'>'{{i['cum_confirmed']}}'</td><td class='freq'>'{{i['cum_active']}}'</td><td class='freq'>'{{i['cum_recovered']}}'</td></tr><tfoot><tr><td></td><td></td><td></td><td></td></tr></tfoot>"
-print(f1_data['Andaman and Nicobar Islands'])
+# tav = "<tr><td>{{ day }}</td><td class='freq'>'{{i['cum_confirmed']}}'</td><td class='freq'>'{{i['cum_active']}}'</td><td class='freq'>'{{i['cum_recovered']}}'</td></tr><tfoot><tr><td></td><td></td><td></td><td></td></tr></tfoot>"
+
 @app.route('/')
 def line():
-    return render_template('graph.html', title='Covid19',table1 = data,table2 = fdata,total = total,st = st,gdata = g_data,g1data = g1_data,fdata = f_data,f1data = f1_data,tav = tav)
+    return render_template('graph.html', title='Covid19',table1 = data,table2 = fdata,total = total,st = st,gdata = g_data,g1data = g1_data,fdata = f_data,f1data = f1_data)
 
 if __name__ == '__main__':
     app.run()
